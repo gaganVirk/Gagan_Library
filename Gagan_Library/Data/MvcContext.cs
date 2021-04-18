d37bd3ea926a22ddd757fe71e117036190bfcc36
gaganVirk <deep.virk@outlook.co.nz>
2021-04-19 10:22:20 +1200

    Book model and context class

---
+++ Gagan_Library/Data/MvcContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gagan_Library.Data
+{
+    public class MvcContext
+    {
+    }
+}

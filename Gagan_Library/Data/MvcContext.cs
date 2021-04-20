0315b0f05747b294a6427834af36d8004cd1a092
gaganVirk <deep.virk@outlook.co.nz>
2021-04-20 20:52:00 +1200

    scafolded movie pages and updated database

--- Gagan_Library/Data/MvcContext.cs
+++ Gagan_Library/Data/MvcContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Gagan_Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Gagan_Library.Data
 {
-    public class MvcContext
+    public class MvcContext : DbContext
     {
+        public MvcContext(DbContextOptions<MvcContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
     }
 }
---
+++ Gagan_Library/Migrations/20210420084346_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gagan_Library.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Genre = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Year = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
